ddc05c3676a309e768d33dd4ae63c11c47d9514d
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2022-11-05 04:51:57 +0900

    파이썬 week9 실습

---
+++ week9/Ex09_02.py
@@ -0,0 +1,16 @@
+num_list = [1, 9, 2, 3, 4, 5, 7]
+
+num_list.sort()
+print(num_list)
+
+del num_list[-1]
+print(num_list)
+
+num_list.insert(5, 6)
+print(num_list)
+
+num_list.append(8)
+print(num_list)
+
+num_list.reverse()
+print(num_list)
